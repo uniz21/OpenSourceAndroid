df2a83cf27c35209efe595137518b350a21797c8
JihyeKim0923 <sos13313@naver.com>
2020-05-31 16:23:30 +0900

    second

--- app/src/main/java/com/example/yoony/opensourceandroidproject/MainActivity.java
+++ app/src/main/java/com/example/yoony/opensourceandroidproject/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.yoony.opensourceandroidproject;
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.yoony.opensourceandroidproject.db.GoalDataTask;
+import com.example.yoony.opensourceandroidproject.db.factory.GoalDAOFactory;
+import com.example.yoony.opensourceandroidproject.db.model.Goal;
+import com.tedpark.tedonactivityresult.rx2.TedRxOnActivityResult;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,8 +32,6 @@ public class MainActivity extends AppCompatActivity
     RecyclerView recyclerView;
     RecyclerAdapter recyclerAdapter;
     ImageButton btnAdd;
-
-    List<Goal> goalList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,32 +55,77 @@ public class MainActivity extends AppCompatActivity
         final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        drawer_open.setOnClickListener(new View.OnClickListener(){
+        drawer_open.setOnClickListener(new View.OnClickListener() {
             @Override
-            public  void onClick(View v){
+            public void onClick(View v) {
                 drawer.openDrawer(navigationView);
             }
         });
 
-        goalList=new ArrayList<>();
 
-        recyclerView=findViewById(R.id.recyclerview);
+        recyclerView = findViewById(R.id.recyclerview);
 
-        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(MainActivity.this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(MainActivity.this);
         recyclerView.setLayoutManager(linearLayoutManager);
 
-        recyclerAdapter=new RecyclerAdapter(goalList);
+        recyclerAdapter = new RecyclerAdapter() {
+            @Override
+            void setSelected(Goal goal) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                intent.putExtra("EXTRA_GOAL", goal);
+                startActivity(intent);
+            }
+        };
         recyclerView.setAdapter(recyclerAdapter);
-        btnAdd=findViewById(R.id.btnAdd);
+        btnAdd = findViewById(R.id.btnAdd);
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //목표 추가하기
-                Intent intent=new Intent(MainActivity.this,AddActivity.class);
-                startActivityForResult(intent,0);
+//                Intent intent=new Intent(MainActivity.this,AddActivity.class);
+//                startActivityForResult(intent,0);
+
+                TedRxOnActivityResult.with(MainActivity.this)
+                        .startActivityForResult(new Intent(getApplicationContext(), AddActivity.class))
+                        .subscribe(activityResult -> {
+                            L.i("[TedRxOnActivityResult] " + activityResult);
+                            if (activityResult.getResultCode() == RESULT_OK) {
+//                                Intent data = activityResult.getData();
+                                onGoalDataLoad();
+                            }
+
+                        }, Throwable::printStackTrace);
             }
         });
+
+        onGoalDataLoad();
+
+    }
+
+    private void onGoalDataLoad() {
+        //쓰레드를 이용하여 데이터를 호출한다.. 데이터호출로직은 백그라운드에서 처리되어야한다.
+        GoalDataTask task = new GoalDataTask.Builder().setFetcher(new GoalDataTask.DataFetcher() {
+            @Override
+            public List<com.example.yoony.opensourceandroidproject.db.model.Goal> getData() {
+                try {
+                    //getGoalList 함수를 통해 최종목표 리스트들을 호출한다.
+                    return GoalDAOFactory.getGoalList(getApplicationContext());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+        }).setCallback(new GoalDataTask.TaskListener() {
+            @Override
+            public void onComplete(List<com.example.yoony.opensourceandroidproject.db.model.Goal> data) {
+                L.i("::::[onGoalDataLoad] CallBack " + data);
+                if (data != null) {
+                    recyclerAdapter.updateItems(data);
+                }
+
+            }
+        }).build();
+        task.execute();
     }
 
     @Override
@@ -116,66 +162,74 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-/***************************************************************************************************목표세우기 추가*/
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode==0){
-            String strMain=data.getStringExtra("main");
+    /***************************************************************************************************목표세우기 추가*/
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if(requestCode==0){
+//            String strMain=data.getStringExtra("main");
+//
+//            Goal goal=new Goal(strMain,0);
+//            recyclerAdapter.addItem(goal);
+//            recyclerAdapter.notifyDataSetChanged();
+//        }
+//    }
 
-            Goal goal=new Goal(strMain,0);
-            recyclerAdapter.addItem(goal);
-            recyclerAdapter.notifyDataSetChanged();
-        }
-    }
-
-    class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder>{
+    abstract class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
+        abstract void setSelected(Goal goal);
 
         private List<Goal> listdata;
 
-        public RecyclerAdapter(List<Goal> listdata){
-            this.listdata=listdata;
+        public RecyclerAdapter() {
+
         }
+
+        public void updateItems(List<Goal> items) {
+            if (this.listdata == null) {
+                listdata = new ArrayList<>();
+            }
+            this.listdata.clear();
+            this.listdata.addAll(items);
+
+            notifyDataSetChanged();
+        }
+
+        private AdapterView.OnItemClickListener mListener = null;
 
         @NonNull
         @Override
         public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {//recycler 빼기
-            View view=LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item,viewGroup,false);
+            View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
             return new ItemViewHolder(view);
         }
 
         @Override
-        public int getItemCount(){
-            return listdata.size();
+        public int getItemCount() {
+            return listdata == null ? 0 : listdata.size();
         }
 
         @Override
         public void onBindViewHolder(@NonNull RecyclerAdapter.ItemViewHolder itemViewHolder, int i) {
-            Goal goal=listdata.get(i);
+            Goal goal = listdata.get(i);
 
-            itemViewHolder.maintext.setText(goal.getMaintext());
+            itemViewHolder.maintext.setText(goal.getGoalTitle());
+
+            //Main 목표의 각 Item 하나씩 클릭할떄마다 발생하는 클릭이벤트
+            itemViewHolder.itemView.setOnClickListener(view -> setSelected(goal));
 
         }
 
-        void addItem(Goal goal){
-            listdata.add(goal);
-        }
-
-        void removeItem(int position){
-            listdata.remove(position);
-        }
-
-        class ItemViewHolder extends RecyclerView.ViewHolder{
+        class ItemViewHolder extends RecyclerView.ViewHolder {
 
             private TextView maintext;
-            private ImageView img;
 
-            public ItemViewHolder(@NonNull View itemView){
+            public ItemViewHolder(@NonNull View itemView) {
                 super(itemView);
 
-                maintext=itemView.findViewById(R.id.item_maintext);
-                img=itemView.findViewById(R.id.item_image);
+                maintext = itemView.findViewById(R.id.item_maintext);
+
             }
         }
     }
--- app/src/main/java/com/example/yoony/opensourceandroidproject/SplashActivity.java
+++ app/src/main/java/com/example/yoony/opensourceandroidproject/SplashActivity.java
@@ -4,12 +4,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.yoony.opensourceandroidproject.db.CommonDAOFactory;
+
 public class SplashActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         try {
             Thread.sleep(1000);
+            //데이터베이스 import
+            CommonDAOFactory.executeCreate(this);
         }catch (InterruptedException e){
             e. printStackTrace();
         }
