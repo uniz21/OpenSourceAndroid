be22f5b0852cd37538a975d61fddabcdfbe87fe9
Zzangeun <tkddms4199@gmail.com>
2020-06-01 15:03:48 +0900

    주현꺼까지 합친거
    상점 + 목표들

--- app/src/main/java/com/example/yoony/opensourceandroidproject/MainActivity.java
+++ app/src/main/java/com/example/yoony/opensourceandroidproject/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.yoony.opensourceandroidproject;
 
 import android.os.Bundle;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -18,28 +20,28 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         setTitle("당근과 채찍");
 
-        ImageButton drawer_open = (ImageButton) findViewById(R.id.drawer_open);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });*/
-
-        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-
-        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        drawer_open.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public  void onClick(View v){
-                drawer.openDrawer(navigationView);
-            }
         });
+         */
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -52,6 +54,28 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -59,15 +83,19 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_main) {
-            // Handle the main action
+            // Handle the camera action
         } else if (id == R.id.nav_todo) {
 
         } else if (id == R.id.nav_goals) {
-
-        } else if (id == R.id.nav_community) {
-
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            FragmentGoals fragmentgoals = new FragmentGoals();
+            transaction.replace(R.id.main_fragment, fragmentgoals);
+            transaction.commit();
         } else if (id == R.id.nav_shop) {
-
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            FragmentShop fragmentshop = new FragmentShop();
+            transaction.replace(R.id.main_fragment, fragmentshop);
+            transaction.commit();
         } else if (id == R.id.nav_setting) {
 
         }
