5e878f34884a633bcb98d5b9bd8b6a8a7bdc3343
uniz21 <yoonyj21@naver.com>
2020-06-11 02:55:52 +0900

    할일 날짜+추가버튼 UI

--- app/src/main/java/com/example/yoony/opensourceandroidproject/FragmentTodo.java
+++ app/src/main/java/com/example/yoony/opensourceandroidproject/FragmentTodo.java
@@ -1,10 +1,13 @@
 package com.example.yoony.opensourceandroidproject;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 
@@ -29,7 +33,8 @@ public class FragmentTodo extends Fragment {
 
     LinearLayout tabLayout,btnLayout;
     Spinner monthSpinner;
-    Button addListBtn,addBtn;
+    Button addBtn;
+    ImageButton addListBtn;
 
     private static final String FRAGMENT_TAG = "FRAGMENT_TAG";
     private static final String KEY_NUMBER = "KEY_NUMBER";
@@ -98,7 +103,7 @@ public class FragmentTodo extends Fragment {
             }
         });
 
-        addListBtn=(Button)view.findViewById(R.id.addListBtn);
+        addListBtn=(ImageButton)view.findViewById(R.id.addListBtn);
         addListBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -117,10 +122,26 @@ public class FragmentTodo extends Fragment {
             Button btn = new Button(view.getContext());//버튼 생성
             btn.setText((i+1)+"일");
             btn.setId((thisMonth*100)+(i+1));
+            btn.setBackground(ContextCompat.getDrawable(view.getContext(),R.drawable.day_btn));
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     callFragment(v.getId());
+                    btn.setFocusableInTouchMode(true);
+                    btn.requestFocus();
+                    Log.e("test","focus"+btn.isFocused());
+                }
+            });
+            btn.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if(hasFocus){
+                        v.setBackground(ContextCompat.getDrawable(view.getContext(),R.drawable.selecte_day_btn));
+                        btn.setTextColor(Color.WHITE);
+                    }else {
+                        btn.setBackground(ContextCompat.getDrawable(view.getContext(),R.drawable.day_btn));
+                        btn.setTextColor(Color.BLACK);
+                    }
                 }
             });
             tabWidgetLayout.addView(btn);
